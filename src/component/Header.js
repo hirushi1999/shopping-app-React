c12de1e2083fb2c4432d7f25d2cf34462809607d
Hirushi <hirushi_n@epiclanka.net>
2024-02-10 11:49:20 +0530

    Developed responsive navigation bar

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,13 +1,33 @@
-import React from 'react'
-import logo from "../asset/logo1.png"
+import React from 'react';
+import { Link } from "react-router-dom";
+import logo from "../asset/logo1.png";
+import { FaUser } from "react-icons/fa6";
+import { PiShoppingCartSimpleFill } from "react-icons/pi";
 
 const Header = () => {
   return (
-    <header className='fixed shadow-md w-full h-16 px-2 md:px-4'>
+    <header className='fixed shadow-md w-full h-14 px-2 md:px-4'>
         {/* desktop */}
-        <div className='flex items-center h-full'>
-            <div className='h-16'>
-                <img src = { logo } className='h-full'/>
+        <div className='flex items-center h-full justify-between'>
+            <Link to={""}>
+              <div className='h-14'>
+                  <img src = { logo } className='h-full'/>
+              </div>
+            </Link>
+
+            <div className='flex items-center gap-4 md:gap-7'>
+              <nav className='flex gap-4 md:gap-7 text-base md:text-md font-medium'>
+                <Link to={""}>HOME</Link>
+                <Link to={"cakes"}>CAKES</Link>
+                <Link to={"about"}>ABOUT US</Link>
+                <Link to={"contact"}>CONTACT US</Link>
+              </nav>
+              <div className='text-1.9xl text-slate-600'>
+                <PiShoppingCartSimpleFill />
+              </div>
+              <div className='text-1.5xl text-slate-600'>
+                <FaUser />
+              </div>
             </div>
         </div>
 
