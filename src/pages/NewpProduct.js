6569b92f74fb1b5b5272707c521f5526c8de39b1
Hirushi <hirushi_n@epiclanka.net>
2024-08-04 19:06:51 +0530

    Create new product page

--- src/pages/NewpProduct.js
+++ src/pages/NewpProduct.js
@@ -1,9 +1,146 @@
-import React from 'react'
+import React, { useState } from "react";
+import { IoMdCloudUpload } from "react-icons/io";
+import { ImagetoBase64 } from "../utility/ImagetoBase64";
+import { toast } from "react-hot-toast";
 
 const NewpProduct = () => {
-  return (
-    <div>NewpProduct</div>
-  )
-}
+  const [data, setData] = useState({
+    name: "",
+    category: "",
+    image: "",
+    price: "",
+    description: "",
+  });
 
-export default NewpProduct
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setData((preve) => {
+      return {
+        ...preve,
+        [name]: value,
+      };
+    });
+  };
+
+  const uploadImage = async (e) => {
+    const data = await ImagetoBase64(e.target.files[0]);
+    setData((preve) => {
+      return {
+        ...preve,
+        image: data,
+      };
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(data);
+
+    const { name, image, category, price } = data;
+    if (name && image && category && price) {
+      const fetchData = await fetch(
+        `${process.env.REACT_APP_SERVER_DOMAIN}/saveProduct`,
+        {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
+
+      const fetchRes = await fetchData.json();
+      console.log(fetchRes);
+      toast(fetchRes.message);
+
+      setData(() => {
+        return {
+          name: "",
+          category: "",
+          image: "",
+          price: "",
+          description: "",
+        };
+      });
+    } else {
+      toast("Enter required fields.");
+    }
+  };
+
+  return (
+    <div className="p-4">
+      <form
+        className="m-auto w-full max-w-md shadow flex flex-col p-3 bg-white"
+        onSubmit={handleSubmit}
+      >
+        <label htmlFor="name">Name</label>
+        <input
+          type={"text"}
+          name="name"
+          className="bg-slate-200 p-1 my-1"
+          onChange={handleOnChange}
+        />
+
+        <label htmlFor="category">Category</label>
+        <select
+          className="bg-slate-200 p-1 my-1"
+          id="category"
+          name="category"
+          onChange={handleOnChange}
+        >
+          <option value={"other"}>--Select Category--</option>
+          <option value={"bdayCakes"}>Birthday Cakes</option>
+          <option value={"wedCakes"}>Wedding Cakes</option>
+          <option value={"cupCakes"}>Cupcakes</option>
+          <option value={"brownies"}>Brownies & Cookies</option>
+          <option value={"season"}>Seasonal Cakes</option>
+          <option value={"classic"}>Classic Cakes</option>
+        </select>
+
+        <label htmlFor="image">
+          Image
+          <div className="h-40 w-full bg-slate-200 rounded flex items-center justify-center cursor-pointer">
+            {data.image ? (
+              <img src={data.image} className="h-full" />
+            ) : (
+              <span className="text-5xl">
+                <IoMdCloudUpload />
+              </span>
+            )}
+            <input
+              type={"file"}
+              accept="image/*"
+              id="image"
+              onChange={uploadImage}
+              className="hidden"
+            />
+          </div>
+        </label>
+
+        <label htmlFor="price" className="my-1">
+          Price
+        </label>
+        <input
+          type={"text"}
+          className="bg-slate-200 p-1 my-1"
+          name="price"
+          onChange={handleOnChange}
+        />
+
+        <label htmlFor="description">Description</label>
+        <textarea
+          rows={2}
+          name="description"
+          className="bg-slate-200 p-1 my-1 resize-none"
+          onChange={handleOnChange}
+        />
+
+        <button className="bg-blue-400 hover:bg-blue-600 cursor-pointer text-lg font-medium text-white drop-shadow my-2">
+          Save
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default NewpProduct;
